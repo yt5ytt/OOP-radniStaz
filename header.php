fd0f226140cb9dd8d848db83b437bb8a13f7aab2
yt5ytt <alexwebsoft.net@gmail.com>
2021-04-21 14:21:01 +0200

    SQL update

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
     <head>
         <title>Radni staz - Testni projekat</title>
         <link rel="stylesheet" type="text/css" href="<?php echo ROOTURI . 'inc/css/master.css'; ?>" />
-        <meta http-equiv="refresh" content="5" />
+        <meta http-equiv="refresh" content="10" />
     </head>
 
     <body>
